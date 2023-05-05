9dbad4b82f8be4e4384b34aee287c2148f6d0a61
Warze <contact@warze.org>
2023-05-05 09:28:52 +0200

    Img preview, expanded on example

--- src/main.js
+++ src/main.js
@@ -1,29 +1,44 @@
 const toolInput = document.getElementById('toolinput');
 const generated = document.getElementById('generated');
 
-const img = new Image();
-img.src = 'images/steve.png';
 
-img.onload = () => {
-    fetch('mappings/mctopdown.json').then((response) => {
-        response.json().then((obj) => {
-            toolInput.value = JSON.stringify(obj, null, '    ');
-            generate(obj);
-        })
+fetch('mappings/mctopdown.json').then((response) => {
+    response.json().then((obj) => {
+        toolInput.value = JSON.stringify(obj, null, '    ');
+        const img = new Image();
+        img.src = obj.inputimage.default;
+        img.onload = () => {
+            generate(obj, img);
+        }
     })
-}
+})
 
-function generate(settings) {
-    const cvs = document.createElement('canvas');
-    const ctx = cvs.getContext('2d');
-
-    cvs.width = settings.canvas.width;
-    cvs.height= settings.canvas.height;
-    ctx.imageSmoothingEnabled = settings.canvas.smooth;
+function generate(settings, img) {
+    generated.appendChild(img)
     
-    settings.pastes.forEach((p) => {
-        ctx.drawImage(img, p.sx, p.sy, p.sw, p.sh, p.dx, p.dy, p.dw, p.dh)
+    settings.pastes.forEach((singleCanvas) => {
+        const cvs = document.createElement('canvas');
+        const ctx = cvs.getContext('2d');
+    
+        cvs.width = settings.canvas.width;
+        cvs.height= settings.canvas.height;
+        ctx.imageSmoothingEnabled = settings.canvas.smooth;
+
+        singleCanvas.forEach((p) => {
+            if (typeof p == 'object') {
+                ctx.translate(p.dx + p.dw / 2, p.dy + p.dh / 2)
+                if (Math.abs(p.dr % 2) == 1) {
+                    const temp = p.dh;
+                    p.dh = p.dw;
+                    p.dw = temp;
+                }
+                ctx.rotate(p.dr * Math.PI / 2)
+                ctx.drawImage(img, p.sx, p.sy, p.sw, p.sh, -p.dw / 2, -p.dh / 2, p.dw, p.dh)
+                ctx.setTransform(1, 0, 0, 1, 0, 0);
+            }
+        })
+
+        generated.appendChild(cvs);
     })
 
-    generated.appendChild(cvs);
 }
